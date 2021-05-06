f1cb090607357f5b1b291df6955de5559cec2f05
Young Hoon Park <vparka@sday.me>
2021-05-06 20:58:33 +0900

    채팅 주소 변경

--- src/modules/langkr.js
+++ src/modules/langkr.js
@@ -263,7 +263,7 @@ export const langkr = {
                 time : '05.25(화) 13:40~15:00',
                 video_text : '한국 문화예술교육 실천가‧교육가‧기관별 사례를 통해 예술교육의 위기를 창조적으로 돌파하는 방법과 사회 위기 해결에 대안을 제시할 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1003&l=kr',
+                chatlink : '/chat/index.html?r=1003&l=kr',
                 list : [
                     {
                         moder : '',
@@ -358,7 +358,7 @@ export const langkr = {
                 time : '05.25(화) 15:00~16:30',
                 video_text : '현대 사회가 유발하고 있는 많은 심리적‧정신적 질병에 대하여, 자아를 새롭게 발견하고, 사회와 건강하게 관계를 맺을 수 있는 치유를 예술적 개입을 통하여 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1004&l=kr',
+                chatlink : '/chat/index.html?r=1004&l=kr',
                 list : [
                     {
                         moder : '',
@@ -438,7 +438,7 @@ export const langkr = {
                 time : '05.25(화) 15:00~16:30',
                 video_text : '전세계적으로 발행하고 있는 평등‧다문화‧고립 등 사회적 도전 과제에 대해 예술교육이 창조적으로 개입할 수 있는 방법을 모색하고, 이를 통해 더욱 포용적인 사회와 문화를 건설할 수 있도록 기여합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1005&l=kr',
+                chatlink : '/chat/index.html?r=1005&l=kr',
                 list : [
                     {
                         moder : '',
@@ -518,7 +518,7 @@ export const langkr = {
                 time : '05.25(화) 16:30~17:50',
                 video_text : '“서울 어젠다: 예술교육 발전목표”가 천명하고 있는 것처럼, 예술교육의 중요성과 가치가 오늘날 현대 사회에 어떤 영향력을 발휘할 수 있는지에 대해 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1006&l=kr',
+                chatlink : '/chat/index.html?r=1006&l=kr',
                 list : [
                     {
                         moder : '',
@@ -616,7 +616,7 @@ export const langkr = {
                 time : '05.26(수) 13:40~15:15',
                 video_text : '판데믹 상황 속에서 위기에 놓인 학교 및 학생들이 예술교육을 통해 회복되고, 다양한 예술 분과 교육들이 학생들을 지속적으로 지지하고 지원할 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1007&l=kr',
+                chatlink : '/chat/index.html?r=1007&l=kr',
                 list : [
                     {
                         moder : '',
@@ -704,7 +704,7 @@ export const langkr = {
                 time : '05.26(수) 15:15~17:05',
                 video_text : '예술교육이 오늘날의 위기에 대항하여 지역 사회가 창조적으로 대응하고 역량을 강화할 수 있도록 도울 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1008&l=kr',
+                chatlink : '/chat/index.html?r=1008&l=kr',
                 list : [
                     {
                         moder : '',
@@ -808,7 +808,7 @@ export const langkr = {
                 time : '05.26(수) 15:15~16:55',
                 video_text : '예술교육을 통해 사회에서 소외된 주체들을 드러내고 이들의 목소리를 발굴하며, 이 과정을 통해 사회를 더욱 다양하고 포용적인 공동체로 이루는 과정을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1009&l=kr',
+                chatlink : '/chat/index.html?r=1009&l=kr',
                 list : [
                     {
                         moder : '',
@@ -904,7 +904,7 @@ export const langkr = {
                 time : '05.26(수) 17:05~18:25',
                 video_text : '한국 문화예술교육 실천가‧교육가‧기관별 사례를 통해 예술교육의 위기를 창조적으로 돌파하는 방법과 사회 위기 해결에 대안을 제시할 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1010&l=kr',
+                chatlink : '/chat/index.html?r=1010&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1335,7 +1335,7 @@ export const langkr = {
                 time : '05.25(화) 13:40~15:00',
                 video_text : '한국 문화예술교육 실천가‧교육가‧기관별 사례를 통해 예술교육의 위기를 창조적으로 돌파하는 방법과 사회 위기 해결에 대안을 제시할 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1003&l=kr',
+                chatlink : '/chat/index.html?r=1003&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1430,7 +1430,7 @@ export const langkr = {
                 time : '05.25(화) 15:00~16:30',
                 video_text : '현대 사회가 유발하고 있는 많은 심리적‧정신적 질병에 대하여, 자아를 새롭게 발견하고, 사회와 건강하게 관계를 맺을 수 있는 치유를 예술적 개입을 통하여 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1004&l=kr',
+                chatlink : '/chat/index.html?r=1004&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1510,7 +1510,7 @@ export const langkr = {
                 time : '05.25(화) 15:00~16:30',
                 video_text : '전세계적으로 발행하고 있는 평등‧다문화‧고립 등 사회적 도전 과제에 대해 예술교육이 창조적으로 개입할 수 있는 방법을 모색하고, 이를 통해 더욱 포용적인 사회와 문화를 건설할 수 있도록 기여합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1005&l=kr',
+                chatlink : '/chat/index.html?r=1005&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1590,7 +1590,7 @@ export const langkr = {
                 time : '05.25(화) 16:30~17:50',
                 video_text : '“서울 어젠다: 예술교육 발전목표”가 천명하고 있는 것처럼, 예술교육의 중요성과 가치가 오늘날 현대 사회에 어떤 영향력을 발휘할 수 있는지에 대해 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1006&l=kr',
+                chatlink : '/chat/index.html?r=1006&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1688,7 +1688,7 @@ export const langkr = {
                 time : '05.26(수) 13:40~15:15',
                 video_text : '판데믹 상황 속에서 위기에 놓인 학교 및 학생들이 예술교육을 통해 회복되고, 다양한 예술 분과 교육들이 학생들을 지속적으로 지지하고 지원할 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1007&l=kr',
+                chatlink : '/chat/index.html?r=1007&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1776,7 +1776,7 @@ export const langkr = {
                 time : '05.26(수) 15:15~17:05',
                 video_text : '예술교육이 오늘날의 위기에 대항하여 지역 사회가 창조적으로 대응하고 역량을 강화할 수 있도록 도울 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1008&l=kr',
+                chatlink : '/chat/index.html?r=1008&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1880,7 +1880,7 @@ export const langkr = {
                 time : '05.26(수) 15:15~16:55',
                 video_text : '예술교육을 통해 사회에서 소외된 주체들을 드러내고 이들의 목소리를 발굴하며, 이 과정을 통해 사회를 더욱 다양하고 포용적인 공동체로 이루는 과정을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1009&l=kr',
+                chatlink : '/chat/index.html?r=1009&l=kr',
                 list : [
                     {
                         moder : '',
@@ -1976,7 +1976,7 @@ export const langkr = {
                 time : '05.26(수) 17:05~18:25',
                 video_text : '한국 문화예술교육 실천가‧교육가‧기관별 사례를 통해 예술교육의 위기를 창조적으로 돌파하는 방법과 사회 위기 해결에 대안을 제시할 수 있는 방법을 모색합니다.',
                 download : false,
-                chatlink : 'http://dc2020.dellang.com:8082/chat/index.html?r=1010&l=kr',
+                chatlink : '/chat/index.html?r=1010&l=kr',
                 list : [
                     {
                         moder : '',
